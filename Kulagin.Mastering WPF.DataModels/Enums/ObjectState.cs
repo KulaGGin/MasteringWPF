36ca0804f52fc4d57cf40c0526aaf63e3727a7a6
KulaGGin <sergeant.coolagin@gmail.com>
2020-05-09 19:53:05 +0300

    Add ObjectState enum.

---
+++ Kulagin.Mastering WPF.DataModels/Enums/ObjectState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.Mastering_WPF.DataModels.Enums {
+    public enum ObjectState {
+        
+        Active = 0,
+        Inactive = 1,
+        Deleted = 2
+    }
+}
