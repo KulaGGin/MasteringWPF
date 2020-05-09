e22979e4bdea4c40bafa6738fbff9ca352922963
KulaGGin <sergeant.coolagin@gmail.com>
2020-05-09 19:30:39 +0300

    Add BaseVisibilityConverter class.

---
+++ Kulagin.Mastering WPF.Converters/BaseVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+
+namespace Kulagin.Mastering_WPF.Converters {
+    
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public abstract class BaseVisibilityConverter {
+
+        protected Visibility FalseVisibilityValue { get; set; } = Visibility.Collapsed;
+
+
+        public enum FalseVisibility {
+            Hidden,
+            Collapsed
+        }
+
+
+        public FalseVisibility FalseVisibilityState {
+            get { return FalseVisibilityValue == Visibility.Collapsed ? FalseVisibility.Collapsed : FalseVisibility.Hidden; }
+            set { FalseVisibilityValue = value == FalseVisibility.Collapsed ? Visibility.Collapsed : Visibility.Hidden; }
+        }
+
+
+        public bool IsInverted { get; set; }
+    }
+}
