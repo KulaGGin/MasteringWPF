41b9c66924cf5bdf390467ab8796b12e2477c141
KulaGGin <sergeant.coolagin@gmail.com>
2020-05-09 19:35:59 +0300

    Add FeedbackType enum.

---
+++ Kulagin.Mastering WPF.DataModels/Enums/FeedbackType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.Mastering_WPF.DataModels.Enums {
+    public enum FeedbackType {
+        None = -1,
+        Error,
+        Information,
+        Question,
+        Success,
+        Validation,
+        Warning
+    }
+}
