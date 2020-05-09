0637fbf977b0ffac001b33a2444e6387ea388006
KulaGGin <sergeant.coolagin@gmail.com>
2020-05-09 19:13:21 +0300

    Add BaseDataModel class.

---
+++ Kulagin.Mastering WPF.DataModels/BaseDataModel.cs
@@ -0,0 +1,32 @@
+﻿using Kulagin.Mastering_WPF.Extensions;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace Kulagin.Mastering_WPF.DataModels {
+    public abstract class BaseDataModel : INotifyPropertyChanged {
+
+        protected BaseDataModel() {
+        }
+
+
+        public abstract override string ToString();
+
+
+        #region INotifyPropertyChanged Members
+
+        public virtual event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected virtual void NotifyPropertyChanged(params string[] propertyNames) {
+            if (PropertyChanged != null) propertyNames.ForEach(p => PropertyChanged(this, new PropertyChangedEventArgs(p)));
+        }
+
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
