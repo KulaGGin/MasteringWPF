c47590d0b84dff32157ef45486c573d6611edd79
KulaGGin <sergeant.coolagin@gmail.com>
2020-05-09 18:20:35 +0300

    Add BaseBusinessViewModel class.

---
+++ Kulagin.Mastering WPF.ViewModels/Business/BaseBusinessViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace Kulagin.Mastering_WPF.ViewModels.Business {
+    public class BaseBusinessViewModel {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected void NotifyPropertyChanged(params string[] propertyNames) {
+            if (PropertyChanged != null) {
+                foreach (string propertyName in propertyNames) {
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+        }
+
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
