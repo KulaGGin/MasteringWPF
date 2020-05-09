a8c63a5bb9af08fba642664e7c056c81ecc701f3
KulaGGin <sergeant.coolagin@gmail.com>
2020-05-09 20:10:15 +0300

    Edit ISynchronisableDataModel interface.
    Edit ISynchronisableDataModel interface to be able to use BaseSynchronizableCollection class.

--- Kulagin.Mastering WPF.DataModels/Interfaces/ISynchronisableDataModel.cs
+++ Kulagin.Mastering WPF.DataModels/Interfaces/ISynchronisableDataModel.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Kulagin.Mastering_WPF.DataModels.Enums;
+
 
 namespace Kulagin.Mastering_WPF.DataModels.Interfaces {
     public interface ISynchronizableDataModel<T> where T : class, new() {
+        
+        ObjectState ObjectState { get; set; }
+
+        bool HasChanges { get; }
+
+        bool IsSynchronized { get; }
+
+        void RevertState();
+
+        void Synchronize();
+
+        bool PropertiesEqual(T dataModel);
 
         void CopyValuesFrom(T dataModel);
+
+        T Clone();
     }
 }
