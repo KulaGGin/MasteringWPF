130a3c73cd0641d49ecf08adde72cb69ded8dff4
KulaGGin <sergeant.coolagin@gmail.com>
2020-05-10 13:31:24 +0300

    Add MockDataProvider class to Kulagin.Mastering WPF.Models project.

---
+++ Kulagin.Mastering WPF.Models/DataProviders/MockDataProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulagin.Mastering_WPF.DataModels;
+using Kulagin.Mastering_WPF.Models.Interfaces;
+
+
+namespace Kulagin.Mastering_WPF.Models.DataProviders {
+    public class MockDataProvider : IDataProvider {
+        public User GetUser(Guid id) {
+            return new User(id, "James Smith", 25);
+        }
+        public bool SaveUser(User user) {
+            return true;
+        }
+    }
+}
