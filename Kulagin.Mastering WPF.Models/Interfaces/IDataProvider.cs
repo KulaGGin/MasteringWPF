385255eaef7ac98b87d730dc1a92b9bdd4617b05
KulaGGin <sergeant.coolagin@gmail.com>
2020-05-10 13:30:41 +0300

    Add IDataProvider interface.

---
+++ Kulagin.Mastering WPF.Models/Interfaces/IDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Kulagin.Mastering_WPF.DataModels;
+
+
+namespace Kulagin.Mastering_WPF.Models.Interfaces {
+    public interface IDataProvider {
+        User GetUser(Guid id);
+        bool SaveUser(User user);
+    }
+}
