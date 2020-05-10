07b13a36252d8c391cf531a9b070bf5f4b8f3b30
KulaGGin <sergeant.coolagin@gmail.com>
2020-05-10 13:00:34 +0300

    Add Auditable class.

---
+++ Kulagin.Mastering WPF.DataModels/Auditable.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+
+namespace Kulagin.Mastering_WPF.DataModels {
+
+    public class Auditable : BaseDataModel {
+        private DateTime createdOn;
+        private DateTime? updatedOn;
+        private User createdBy, updatedBy;
+
+
+        public Auditable(DateTime createdOn, User createdBy, DateTime? updatedOn, User updatedBy) {
+            CreatedOn = createdOn;
+            CreatedBy = createdBy;
+            UpdatedOn = updatedOn;
+            UpdatedBy = updatedBy;
+        }
+
+
+        public Auditable(User createdBy) : this(DateTime.Now, createdBy, null, new User()) {
+        }
+
+
+        public Auditable() {
+        }
+
+
+        public DateTime CreatedOn {
+            get { return createdOn; }
+            set {
+                createdOn = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public User CreatedBy {
+            get { return createdBy; }
+            set {
+                createdBy = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public DateTime? UpdatedOn {
+            get { return updatedOn; }
+            set {
+                updatedOn = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public User UpdatedBy {
+            get { return updatedBy; }
+            set {
+                updatedBy = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        public override string ToString() {
+            return UpdatedOn.HasValue
+                       ? $"Created on {CreatedOn.ToLongDateString()} by {CreatedBy.Name}, last updated on {UpdatedOn.Value.ToLongDateString()} by {UpdatedBy.Name}"
+                       : $"Created on {CreatedOn.ToLongDateString()} by {CreatedBy.Name}";
+        }
+    }
+}
