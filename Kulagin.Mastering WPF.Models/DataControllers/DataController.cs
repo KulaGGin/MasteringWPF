210dd0bc32c6ec29aa629aa27125dd1369931f90
KulaGGin <sergeant.coolagin@gmail.com>
2020-05-10 13:37:04 +0300

    Add DataController class to Kulagin.Mastering WPF.Models project.

---
+++ Kulagin.Mastering WPF.Models/DataControllers/DataController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Kulagin.Mastering_WPF.DataModels;
+using Kulagin.Mastering_WPF.Models.Interfaces;
+
+
+namespace Kulagin.Mastering_WPF.Models.DataControllers {
+    public class DataController {
+
+        private IDataProvider dataProvider;
+
+
+        public DataController(IDataProvider dataProvider) {
+            DataProvider = dataProvider;
+        }
+
+
+        protected IDataProvider DataProvider { get { return dataProvider; } private set { dataProvider = value; } }
+
+
+        public User GetUser(Guid id) {
+            return DataProvider.GetUser(id);
+        }
+
+
+        public bool SaveUser(User user) {
+            return DataProvider.SaveUser(user);
+        }
+    }
+}
