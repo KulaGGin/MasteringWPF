fa27e0bf78cb61d2b1a0cb9c35bb2a005a588d50
KulaGGin <sergeant.coolagin@gmail.com>
2020-05-10 13:51:09 +0300

    Change startup mechanism of Mastering WPF application.

--- Kulagin.Mastering WPF/App.xaml.cs
+++ Kulagin.Mastering WPF/App.xaml.cs
@@ -5,11 +5,26 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Kulagin.Mastering_WPF.Managers;
+using Kulagin.Mastering_WPF.Models.DataProviders;
+using Kulagin.Mastering_WPF.Models.Interfaces;
+
 
 namespace Kulagin.Mastering_WPF {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        public void App_Startup(object sender, StartupEventArgs e) {
+            RegisterDependencies();
+            new MainWindow().Show();
+        }
+        private void RegisterDependencies() {
+            DependencyManager.Instance.ClearRegistrations();
+            DependencyManager.Instance.Register<IDataProvider, MockDataProvider>();
+            //DependencyManager.Instance.Register<IEmailManager, EmailManager>();
+            //DependencyManager.Instance.Register<IExcelManager, ExcelManager>();
+            //DependencyManager.Instance.Register<IWindowManager, WindowManager>();
+        }
     }
 }
