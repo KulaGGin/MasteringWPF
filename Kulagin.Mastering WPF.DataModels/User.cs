8a6d017f848ca173686607cb6adc5a07d1bb0e1f
KulaGGin <sergeant.coolagin@gmail.com>
2020-05-10 13:27:13 +0300

    Edit User class.
    Add parameterless constructor.

--- Kulagin.Mastering WPF.DataModels/User.cs
+++ Kulagin.Mastering WPF.DataModels/User.cs
@@ -3,13 +3,15 @@
 
 namespace Kulagin.Mastering_WPF.DataModels {
     public class User {
+        public User() { }
+
         public User(Guid id, string name, int age) {
             Id = id;
             Name = name;
             Age = age;
         }
 
-
+        
         public Guid Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
