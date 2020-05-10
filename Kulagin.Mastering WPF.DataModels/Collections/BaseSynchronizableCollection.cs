026da3c49c2c63eaf774d91ce74d592f9519c458
KulaGGin <sergeant.coolagin@gmail.com>
2020-05-10 13:28:53 +0300

    Refactor BaseSynchronizableCollection class.
    Edit blank lines between class members.

--- Kulagin.Mastering WPF.DataModels/Collections/BaseSynchronizableCollection.cs
+++ Kulagin.Mastering WPF.DataModels/Collections/BaseSynchronizableCollection.cs
@@ -9,24 +9,23 @@ using Kulagin.Mastering_WPF.Extensions;
 namespace CompanyName.ApplicationName.DataModels.Collections {
     public class BaseSynchronizableCollection<T> : BaseCollection<T> where T : class, ISynchronizableDataModel<T>, INotifyPropertyChanged, new() {
 
-        
         public BaseSynchronizableCollection(IEnumerable<T> collection) : base(collection) {
         }
 
+        
         public BaseSynchronizableCollection(params T[] collection) : base(collection as IEnumerable<T>) {
         }
 
 
+        
         public BaseSynchronizableCollection() : base() {
         }
 
-
+        
         public virtual bool HasChanges { get { return this.Any(i => i.HasChanges); } }
-
 
         public virtual bool AreSynchronized { get { return this.All(i => i.IsSynchronized); } }
 
-        
         public virtual IEnumerable<T> ChangedCollection { get { return this.Where(i => i.HasChanges); } }
 
 
