db62f2b77640cfc6d2211097e7545fb574f94b81
KulaGGin <sergeant.coolagin@gmail.com>
2020-05-10 16:28:58 +0300

    Add IUserViewModel intefrace.
    Add IUserViewModel intefrace to Kulagin.Mastering WPF.ViewModels project.

---
+++ Kulagin.Mastering WPF.ViewModels/Interfaces/IUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Input;
+using Kulagin.Mastering_WPF.DataModels;
+
+
+namespace Kulagin.Mastering_WPF.ViewModels.Interfaces {
+    public interface IUserViewModel {
+        User User { get; set; }
+
+        ICommand SaveCommand { get; }
+    }
+}
