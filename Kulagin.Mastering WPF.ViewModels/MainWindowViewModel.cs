1e004f021988aca705b9458d7c976399399e4731
KulaGGin <sergeant.coolagin@gmail.com>
2020-06-10 20:06:17 +0300

    Add MainWindowViewModel.

---
+++ Kulagin.Mastering WPF.ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulagin.Mastering_WPF.DataModels;
+
+
+namespace Kulagin.Mastering_WPF.ViewModels {
+    public class MainWindowViewModel {
+        
+
+        public MainWindowViewModel() {
+            
+        }
+
+
+    }
+}
