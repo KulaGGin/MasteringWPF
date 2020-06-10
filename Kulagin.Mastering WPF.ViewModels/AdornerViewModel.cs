d7b68aea649b65eb725f00c16ac4e5d644d06c75
KulaGGin <sergeant.coolagin@gmail.com>
2020-06-10 20:04:57 +0300

    Add AdornerViewModel.

---
+++ Kulagin.Mastering WPF.ViewModels/AdornerViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.Mastering_WPF.ViewModels {
+    /// <summary>
+    /// A View Model that is paired with the AdornerViewModel class, which needs no data or functionality supplied to it.
+    /// </summary>
+    public class AdornerViewModel : BaseViewModel { }
+}
