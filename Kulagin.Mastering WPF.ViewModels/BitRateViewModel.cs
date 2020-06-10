dd93d2b71da0969ff7dd164aaa0d38e3959606ad
KulaGGin <sergeant.coolagin@gmail.com>
2020-06-10 20:05:35 +0300

    Add BitRateViewModel.

---
+++ Kulagin.Mastering WPF.ViewModels/BitRateViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulagin.Mastering_WPF.DataModels.Enums;
+using Kulagin.Mastering_WPF.Extensions;
+
+
+namespace Kulagin.Mastering_WPF.ViewModels {
+    public class BitRateViewModel : BaseViewModel {
+        private ObservableCollection<BitRate> bitRates = new ObservableCollection<BitRate>();
+        private BitRate bitRate = BitRate.Sixteen;
+        public BitRateViewModel() {
+            bitRates.FillWithMembers();
+        }
+        public ObservableCollection<BitRate> BitRates {
+            get { return bitRates; }
+            set {
+                if(bitRates != value) {
+                    bitRates = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public BitRate BitRate {
+            get { return bitRate; }
+            set {
+                if(bitRate != value) {
+                    bitRate = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}
