75dcf88cd40030bae4a0f077deccf27b3aabccb2
KulaGGin <sergeant.coolagin@gmail.com>
2020-06-10 20:06:57 +0300

    Add MenuItemViewModel.

---
+++ Kulagin.Mastering WPF.ViewModels/MenuItemViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace Kulagin.Mastering_WPF.ViewModels {
+    public class MenuItemViewModel : BaseViewModel {
+        private string header = string.Empty;
+        private ICommand command = null;
+        private ObservableCollection<MenuItemViewModel> menuItems =
+            new ObservableCollection<MenuItemViewModel>();
+        public string Header {
+            get { return header; }
+            set {
+                if(header != value) {
+                    header = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public ICommand Command {
+            get { return command; }
+            set {
+                if(command != value) {
+                    command = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public ObservableCollection<MenuItemViewModel> MenuItems {
+            get { return menuItems; }
+            set {
+                if(menuItems != value) {
+                    menuItems = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}
