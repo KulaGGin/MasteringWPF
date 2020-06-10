9ddd7aaa3b890c2f2856757034ad00857d7d0a5a
KulaGGin <sergeant.coolagin@gmail.com>
2020-06-10 19:45:29 +0300

    Add xmlns definition for ViewModels namespace.

--- Kulagin.Mastering WPF.ViewModels/BaseViewModel.cs
+++ Kulagin.Mastering WPF.ViewModels/BaseViewModel.cs
@@ -1,7 +1,8 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Markup;
 
-
+[assembly: XmlnsDefinition("Kulagin/MasteringWPF/ViewModels", "Kulagin.Mastering_WPF.ViewModels")]
 namespace Kulagin.Mastering_WPF.ViewModels {
     public class BaseViewModel : INotifyPropertyChanged {
 
