49f2d6ba3a103a23939ad10c6672d71fa1b6ac9a
KulaGGin <sergeant.coolagin@gmail.com>
2020-06-13 11:38:50 +0300

    Add UserViewModelLocator class.

---
+++ Kulagin.Mastering WPF.Views/ViewModelLocators/UserViewModelLocator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Kulagin.Mastering_WPF.Managers;
+using Kulagin.Mastering_WPF.ViewModels.Interfaces;
+
+
+namespace Kulagin.Mastering_WPF.Views.ViewModelLocators {
+    public class UserViewModelLocator : BaseViewModelLocator<IUserViewModel> {
+        /// <summary>
+        /// Initializes a new UserViewModelLocator control with default values.
+        /// </summary>
+        public UserViewModelLocator() {
+            DesignTimeViewModel = new UserViewModel();
+        }
+    }
+}
