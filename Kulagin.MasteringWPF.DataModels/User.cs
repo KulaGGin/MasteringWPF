ed7ac68be917276a1ed7fb04b68f317a833372a4
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-04 17:38:56 +0300

    Update user class in DataModels project.
    Implement IAutitable interface.

--- Kulagin.MasteringWPF.DataModels/User.cs
+++ Kulagin.MasteringWPF.DataModels/User.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Kulagin.MasteringWPF.DataModels.Interfaces;
+
 
 namespace Kulagin.MasteringWPF.DataModels {
-    public class User {
+    public class User : IAuditable {
+        private Auditable auditable;
+
+        public Auditable Auditable {
+            get { return auditable; }
+            set { auditable = value; }
+        }
     }
 }
