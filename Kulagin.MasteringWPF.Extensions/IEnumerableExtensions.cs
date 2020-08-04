2da4605e69a1973c27ff2bd40691edfe648dde04
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-04 17:51:14 +0300

    Add IEnumerableExtensions class to the Extensions project.

---
+++ Kulagin.MasteringWPF.Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.Extensions {
+    public static class IEnumerableExtensions {
+        public static void ForEach<T>(this IEnumerable<T> collection, Action<T> action) {
+            foreach(T item in collection) {
+                action(item);
+            }
+        }
+    }
+}
