1b3e9863083370a508b8fe4847677d7f733ebd13
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-04 20:28:47 +0300

    Add public static General class with the GetDescription extension method.

---
+++ Kulagin.MasteringWPF.Extensions/General.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.Extensions {
+    public static class General {
+        public static string GetDescription(this Enum value) {
+            FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
+
+            if(fieldInfo == null)
+                return Enum.GetName(value.GetType(), value);
+
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if(attributes != null && attributes.Length > 0)
+                return attributes[0].Description;
+
+            return Enum.GetName(value.GetType(), value);
+        }
+    }
+}
