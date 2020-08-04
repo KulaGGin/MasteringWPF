003d30255a0538bb5041b03fefa6e9218a1ae7b9
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-04 19:58:27 +0300

    Add BoolToVisibilityConverter class to the Converters project.

---
+++ Kulagin.MasteringWPF.Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+
+namespace Kulagin.MasteringWPF.Converters {
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class BoolToVisibilityConverter : BaseVisibilityConverter, IValueConverter {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if(value == null || value.GetType() != typeof(bool))
+                return DependencyProperty.UnsetValue;
+
+            bool boolValue = IsInverted ? !(bool)value : (bool)value;
+
+            return boolValue ? Visibility.Visible : FalseVisibilityValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            if(value == null || value.GetType() != typeof(Visibility))
+                return DependencyProperty.UnsetValue;
+
+            if(IsInverted)
+                return (Visibility)value != Visibility.Visible;
+
+            return (Visibility)value == Visibility.Visible;
+        }
+    }
+}
