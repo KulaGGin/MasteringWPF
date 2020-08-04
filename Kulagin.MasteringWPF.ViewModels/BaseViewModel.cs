2af4857a73d61cfd86bcc180ee5c5770920a7f7a
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-04 14:40:02 +0300

    Add BaseViewModel class to the ViewModels project.

---
+++ Kulagin.MasteringWPF.ViewModels/BaseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.ViewModels {
+    public class BaseViewModel : INotifyPropertyChanged {
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged(params string[] propertyNames) {
+            if (PropertyChanged != null) {
+                foreach (string propertyName in propertyNames) {
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+        }
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+    }
+}
