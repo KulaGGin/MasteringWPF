ac3eec6e8dacc20837596a8abccdadd453b8a5ea
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-04 15:53:02 +0300

    Add Invoice class to the DataModels project.

--- Kulagin.MasteringWPF.DataModels/BaseDataModel.cs
+++ Kulagin.MasteringWPF.DataModels/BaseDataModel.cs
@@ -10,25 +10,6 @@ using Kulagin.MasteringWPF.DataModels.Annotations;
 
 namespace Kulagin.MasteringWPF.DataModels {
     public class BaseDataModel : INotifyPropertyChanged {
-        private DateTime createdOn;
-        private DateTime? updatedOn;
-        private User createdBy, updatedBy;
-        public DateTime CreatedOn {
-            get { return createdOn; }
-            set { createdOn = value; NotifyPropertyChanged(); }
-        }
-        public User CreatedBy {
-            get { return createdBy; }
-            set { createdBy = value; NotifyPropertyChanged(); }
-        }
-        public DateTime? UpdatedOn {
-            get { return updatedOn; }
-            set { updatedOn = value; NotifyPropertyChanged(); }
-        }
-        public User UpdatedBy {
-            get { return updatedBy; }
-            set { updatedBy = value; NotifyPropertyChanged(); }
-        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
---
+++ Kulagin.MasteringWPF.DataModels/Invoice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulagin.MasteringWPF.DataModels.Interfaces;
+
+
+namespace Kulagin.MasteringWPF.DataModels {
+    public class Invoice : BaseDataModel, IAuditable {
+        private DateTime createdOn;
+        private DateTime? updatedOn;
+        private User createdBy, updatedBy;
+        public DateTime CreatedOn {
+            get { return createdOn; }
+            set { createdOn = value; NotifyPropertyChanged(); }
+        }
+        public User CreatedBy {
+            get { return createdBy; }
+            set { createdBy = value; NotifyPropertyChanged(); }
+        }
+        public DateTime? UpdatedOn {
+            get { return updatedOn; }
+            set { updatedOn = value; NotifyPropertyChanged(); }
+        }
+        public User UpdatedBy {
+            get { return updatedBy; }
+            set { updatedBy = value; NotifyPropertyChanged(); }
+        }
+    }
+}
