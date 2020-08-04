e89ad0276b9352fa619cbe7321fa98ad42614d6b
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-04 15:47:24 +0300

    Add IAuditable interface to the DataModels project.

---
+++ Kulagin.MasteringWPF.DataModels/Interfaces/IAuditable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.DataModels.Interfaces {
+    public interface IAuditable {
+        DateTime CreatedOn { get; set; }
+        User CreatedBy { get; set; }
+        DateTime? UpdatedOn { get; set; }
+        User UpdatedBy { get; set; }
+    }
+}
