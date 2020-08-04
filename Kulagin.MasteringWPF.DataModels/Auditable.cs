bd3c4dfe35334f04bd454c3ae899d43c9b7061fa
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-04 15:58:13 +0300

    Add Auditable class to the DataModels project.

---
+++ Kulagin.MasteringWPF.DataModels/Auditable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.DataModels {
+
+    public class Auditable : BaseDataModel {
+        private DateTime createdOn;
+        private DateTime? updatedOn;
+        private User createdBy, updatedBy;
+        public DateTime CreatedOn {
+            get { return createdOn; }
+            set { createdOn = value; NotifyPropertyChanged(); }
+        }
+        public User CreatedBy {
+            get { return createdBy; }
+            set { createdBy = value; NotifyPropertyChanged(); }
+        }
+        public DateTime? UpdatedOn {
+            get { return updatedOn; }
+            set { updatedOn = value; NotifyPropertyChanged(); }
+        }
+        public User UpdatedBy {
+            get { return updatedBy; }
+            set { updatedBy = value; NotifyPropertyChanged(); }
+        }
+    }
+}
