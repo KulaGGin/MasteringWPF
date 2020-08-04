2e379faaa4f297eeda112569836f491b6381eb02
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-04 20:27:03 +0300

    Add BitRate enum to the DataModels project.

---
+++ Kulagin.MasteringWPF.DataModels/Enums/BitRate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.DataModels.Enums {
+    public enum BitRate {
+        [Description("16 bits")]
+        Sixteen = 16,
+        [Description("24 bits")]
+        TwentyFour = 24,
+        [Description("32 bits")]
+        ThirtyTwo = 32,
+    }
+}
