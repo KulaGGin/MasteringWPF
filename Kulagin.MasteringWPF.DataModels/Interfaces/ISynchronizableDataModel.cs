e81d672720eb37f5bef32c0a8ec1d4886ca0bc5e
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-06 13:28:59 +0300

    Add ISynchronizableDataModel interface to the DataModels project.

---
+++ Kulagin.MasteringWPF.DataModels/Interfaces/ISynchronizableDataModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.DataModels.Interfaces {
+    public interface ISynchronizableDataModel<T> where T: class, new() {
+        void CopyValuesFrom(T dataModel);
+    }
+}
