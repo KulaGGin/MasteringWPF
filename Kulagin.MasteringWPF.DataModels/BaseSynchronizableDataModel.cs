bbe215a0400e6275d6b24b0edd8f742078ce47f3
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-06 13:29:16 +0300

    Add BaseSynchronizableDataModel class to the DataModels project.

---
+++ Kulagin.MasteringWPF.DataModels/BaseSynchronizableDataModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulagin.MasteringWPF.DataModels.Interfaces;
+
+
+namespace Kulagin.MasteringWPF.DataModels {
+    public abstract class BaseSynchronizableDataModel<T> : BaseDataModel, ISynchronizableDataModel<T> where T : BaseDataModel, ISynchronizableDataModel<T>, new() {
+        private T originalState;
+        public T OriginalState {
+            get { return originalState; }
+            private set { originalState = value; }
+        }
+
+        public abstract void CopyValuesFrom(T dataModel);
+
+        public virtual T Clone() {
+            T clone = new T();
+            clone.CopyValuesFrom(this as T);
+            return clone;
+        }
+        public abstract bool PropertiesEqual(T dataModel);
+
+        public bool HasChanges {
+            get { return originalState != null && !PropertiesEqual(originalState); }
+        }
+        public void Synchronize() {
+            originalState = this.Clone();
+            NotifyPropertyChanged(nameof(HasChanges));
+        }
+        public void RevertState() {
+            Debug.Assert(originalState != null, "Object not yet synchronized.");
+            CopyValuesFrom(originalState);
+            Synchronize();
+            NotifyPropertyChanged(nameof(HasChanges));
+        }
+    }
+}
