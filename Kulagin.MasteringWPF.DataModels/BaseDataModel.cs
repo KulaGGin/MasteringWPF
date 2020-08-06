0fe23573ab0e9f1ebf5391ecb41fed80d2785b18
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-06 14:31:51 +0300

    Update BaseDataModel class.
    Add NotifyPropertyChanged overload to support multiple property names.

--- Kulagin.MasteringWPF.DataModels/BaseDataModel.cs
+++ Kulagin.MasteringWPF.DataModels/BaseDataModel.cs
@@ -6,16 +6,24 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Kulagin.MasteringWPF.DataModels.Annotations;
+using Kulagin.MasteringWPF.Extensions;
 
 
 namespace Kulagin.MasteringWPF.DataModels {
     public class BaseDataModel : INotifyPropertyChanged {
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        #region INotifyPropertyChanged Members
 
-        [NotifyPropertyChangedInvocator]
-        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = null) {
+        public virtual event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged(params string[] propertyNames) {
+            if(PropertyChanged != null) propertyNames.ForEach(p => PropertyChanged(this, new PropertyChangedEventArgs(p)));
+        }
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "") {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        #endregion
     }
 }
