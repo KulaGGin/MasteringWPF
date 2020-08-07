f34602410de6ccada2132abfe8e494e70d54262c
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-07 16:23:02 +0300

    Add MessageBoxButtonSelection enum to the DataModels project.

---
+++ Kulagin.MasteringWPF.DataModels/Enums/MessageBoxButtonSelection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.DataModels.Enums {
+    /// <summary>
+    /// Specifies which message box button that a user clicks.
+    /// </summary>
+    public enum MessageBoxButtonSelection {
+        /// <summary>
+        /// The message box returns no result.
+        /// </summary>
+        None,
+        /// <summary>
+        /// The result value of the message box is OK.
+        /// </summary>
+        Ok,
+        /// <summary>
+        /// The result value of the message box is Cancel.
+        /// </summary>
+        Cancel,
+        /// <summary>
+        /// The result value of the message box is Yes.
+        /// </summary>
+        Yes,
+        /// <summary>
+        /// The result value of the message box is No.
+        /// </summary>
+        No,
+    }
+}
