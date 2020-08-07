b1fad13932d40bad82be44db1ef502c989c3ce78
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-07 20:34:24 +0300

    Add IUserViewModel to the ViewModels project.

---
+++ Kulagin.MasteringWPF.ViewModels/Interfaces/IUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Kulagin.MasteringWPF.DataModels;
+
+
+namespace Kulagin.MasteringWPF.ViewModels.Interfaces {
+    /// <summary>
+    /// Provides data for the UserView class.
+    /// </summary>
+    public interface IUserViewModel {
+        /// <summary>
+        /// Gets or sets the User object in the related View.
+        /// </summary>
+        User User { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ICommand object for the Save command in the related View.
+        /// </summary>
+        ICommand SaveCommand { get; }
+    }
+}
