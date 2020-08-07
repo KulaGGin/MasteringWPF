4fc1f01e6df7cb336c3acb8d529aa6edf6013b3e
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-07 21:08:16 +0300

    Add ViewModelLocator class to the Views project.

---
+++ Kulagin.MasteringWPF.Views/ViewModelLocators/ViewModelLocator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulagin.MasteringWPF.Managers;
+using Kulagin.MasteringWPF.ViewModels.Interfaces;
+
+
+namespace Kulagin.MasteringWPF.Views.ViewModelLocators {
+    class ViewModelLocator {
+        public IUserViewModel UserViewModel {
+            get { return DependencyManager.Instance.Resolve<IUserViewModel>(); }
+        }
+    }
+}
