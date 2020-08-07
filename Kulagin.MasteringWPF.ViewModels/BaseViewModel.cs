b239c83fc31aed78b548ad551f3e09b2c5e865e0
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-07 13:33:18 +0300

    Update BaseViewModel class.
    Add AddNewDataTypeToCollection, InsertNewDataTypeToCollection, RemoveDataTypeFromCollection methods.

--- Kulagin.MasteringWPF.ViewModels/BaseViewModel.cs
+++ Kulagin.MasteringWPF.ViewModels/BaseViewModel.cs
@@ -5,9 +5,48 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Kulagin.MasteringWPF.DataModels;
+using Kulagin.MasteringWPF.DataModels.Collections;
+using Kulagin.MasteringWPF.DataModels.Interfaces;
+
 
 namespace Kulagin.MasteringWPF.ViewModels {
     public class BaseViewModel : INotifyPropertyChanged {
+
+        public T AddNewDataTypeToCollection<S, T>(S collection)
+            where S : BaseSynchronizableCollection<T>
+            where T : BaseSynchronizableDataModel<T>, new() {
+            T item = collection.GetNewItem();
+            if(item is IAuditable)
+                ((IAuditable)item).Auditable.CreatedOn = DateTime.Now;
+            item.Synchronize();
+            collection.Add(item);
+            collection.CurrentItem = item;
+            return item;
+        }
+
+        public T InsertNewDataTypeToCollection<S, T>(int index, S collection)
+            where S : BaseSynchronizableCollection<T>
+            where T : BaseSynchronizableDataModel<T>, new() {
+            T item = collection.GetNewItem();
+            if(item is IAuditable)
+                ((IAuditable)item).Auditable.CreatedOn = DateTime.Now;
+            item.Synchronize();
+            collection.Insert(index, item);
+            collection.CurrentItem = item;
+            return item;
+        }
+        public void RemoveDataTypeFromCollection<S, T>(S collection, T item)
+            where S : BaseSynchronizableCollection<T>
+            where T : BaseSynchronizableDataModel<T>, new() {
+            int index = collection.IndexOf(item);
+            collection.RemoveAt(index);
+            if(index > collection.Count) index = collection.Count;
+            else if(index < 0) index++;
+            if(index > 0 && index < collection.Count &&
+               collection.CurrentItem != collection[index])
+                collection.CurrentItem = collection[index];
+        }
 
         #region INotifyPropertyChanged Members
 
