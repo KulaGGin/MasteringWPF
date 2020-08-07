41edac842ebf35019374ae2b16cf965627d8a024
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-07 13:57:05 +0300

    Add IDataProvider interface to the Models project.

---
+++ Kulagin.MasteringWPF.Models/Interfaces/IDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulagin.MasteringWPF.DataModels;
+
+
+namespace Kulagin.MasteringWPF.Models.Interfaces {
+    public interface IDataProvider {
+        User GetUser(Guid id);
+        bool SaveUser(User user);
+    }
+}
