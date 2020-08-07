7a9b8d9cfebe362157569bd592c023c6663c2aef
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-07 17:18:19 +0300

    Add MockWindowManager class to the Test.Kulagin.MasteringWPF.Mocks project.

---
+++ Test.Kulagin.MasteringWPF.Mocks/Managers/MockWindowManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulagin.MasteringWPF.DataModels.Enums;
+using Kulagin.MasteringWPF.Managers.Interfaces;
+
+
+namespace Test.Kulagin.MasteringWPF.Mocks.Managers {
+    public class MockWindowManager : IWindowManager {
+        public MessageBoxButtonSelection ShowMessageBox(string message, string title, MessageBoxButton buttons, MessageBoxIcon icon) {
+            switch(buttons) {
+                case MessageBoxButton.Ok:
+                case MessageBoxButton.OkCancel:
+                    return MessageBoxButtonSelection.Ok;
+                case MessageBoxButton.YesNo:
+                case MessageBoxButton.YesNoCancel:
+                    return MessageBoxButtonSelection.Yes;
+                default: return MessageBoxButtonSelection.Ok;
+            }
+        }
+    }
+}
