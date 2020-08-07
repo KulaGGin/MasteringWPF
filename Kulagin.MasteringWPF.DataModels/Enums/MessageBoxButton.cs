8c5671d870b3c44c35427903c32c15a69e7bd867
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-07 17:10:35 +0300

    Add MessageBoxButton enum to the DataModels project.

---
+++ Kulagin.MasteringWPF.DataModels/Enums/MessageBoxButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.DataModels.Enums {
+    /// <summary>
+    /// Specifies the buttons that are displayed on a message box.
+    /// </summary>
+    public enum MessageBoxButton {
+        /// <summary>
+        /// The message box displays an OK button.
+        /// </summary>
+        Ok = 0,
+        /// <summary>
+        /// The message box displays OK and Cancel buttons.
+        /// </summary>
+        OkCancel = 1,
+        /// <summary>
+        /// The message box displays Yes, No, and Cancel buttons.
+        /// </summary>
+        YesNoCancel = 3,
+        /// <summary>
+        /// The message box displays Yes and No buttons.
+        /// </summary>
+        YesNo = 4,
+    }
+}
