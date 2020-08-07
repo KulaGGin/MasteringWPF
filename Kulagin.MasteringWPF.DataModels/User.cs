5f52b6a724f21c8878c2d918e0ae60e9ed125451
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-07 14:35:40 +0300

    Update user class to use in the MockDataProvider class.
    Add Id, Name, Age properties.
    Inherit from BaseSynchronizablaDataModel<User> class. Implement its interface: CopyValuesFrom and PropertiesEqual methods.

--- Kulagin.MasteringWPF.DataModels/User.cs
+++ Kulagin.MasteringWPF.DataModels/User.cs
@@ -7,12 +7,55 @@ using Kulagin.MasteringWPF.DataModels.Interfaces;
 
 
 namespace Kulagin.MasteringWPF.DataModels {
-    public class User : IAuditable {
+    public class User : BaseSynchronizableDataModel<User>, IAuditable {
+        private Guid id = Guid.Empty;
+        private string name = string.Empty;
+        private int age = 0;
+
+        public User(Guid id, string name, int age) {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        public User() { }
+
+        public Guid Id {
+            get { return id; }
+            set { if(id != value) { id = value; NotifyPropertyChanged(); } }
+        }
+
+        public string Name {
+            get { return name; }
+            set { name = value; NotifyPropertyChanged(); }
+        }
+
+        public int Age {
+            get { return age; }
+            set { age = value; NotifyPropertyChanged(); }
+        }
+
         private Auditable auditable;
 
         public Auditable Auditable {
             get { return auditable; }
             set { auditable = value; }
         }
+
+        #region Overrides of BaseSynchronizableDataModel<User>
+
+        public override void CopyValuesFrom(User user) {
+            Id = user.Id;
+            Name = user.Name;
+            Age = user.Age;
+        }
+
+        public override bool PropertiesEqual(User otherUser) {
+            if(otherUser == null) return false;
+            return Id == otherUser.Id && Name == otherUser.Name && Age == otherUser.Age;
+        }
+
+        #endregion
+
     }
 }
