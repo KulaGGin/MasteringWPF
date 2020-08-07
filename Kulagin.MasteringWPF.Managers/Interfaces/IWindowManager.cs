3fec3302ceb68a2edf414873c59bbdf72876ded2
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-07 16:31:38 +0300

    Add IWindowManager interface to the Managers project.

---
+++ Kulagin.MasteringWPF.Managers/Interfaces/IWindowManager.cs
@@ -0,0 +1,7 @@
+﻿using Kulagin.MasteringWPF.DataModels.Enums;
+
+namespace Kulagin.MasteringWPF.Managers.Interfaces {
+    public interface IWindowManager {
+        MessageBoxButtonSelection ShowMessageBox(string message, string title, MessageBoxButton buttons, MessageBoxIcon icon);
+    }
+}
