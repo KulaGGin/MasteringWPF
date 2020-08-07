e199bfe4b34071a900127a6ef48c46bf3202b37d
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-07 16:15:22 +0300

    Update App class.
    Use DependencyManager to register dependencies.

--- Kulagin.MasteringWPF/App.xaml.cs
+++ Kulagin.MasteringWPF/App.xaml.cs
@@ -5,11 +5,26 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Kulagin.MasteringWPF.Managers;
+using Kulagin.MasteringWPF.Models.Interfaces;
+using Test.Kulagin.MasteringWPF.Models.DataProviders;
+
 
 namespace Kulagin.MasteringWPF {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        public void App_Startup(object sender, StartupEventArgs e) {
+            RegisterDependencies();
+            new MainWindow().Show();
+        }
+        private void RegisterDependencies() {
+            DependencyManager.Instance.ClearRegistrations();
+            DependencyManager.Instance.Register<IDataProvider, MockDataProvider>();
+            //DependencyManager.Instance.Register<IEmailManager, EmailManager>();
+            //DependencyManager.Instance.Register<IExcelManager, ExcelManager>();
+            //DependencyManager.Instance.Register<IWindowManager, WindowManager>();
+        }
     }
 }
