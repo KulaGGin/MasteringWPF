5c123bf599b3158ee258619c4c9ac0c11f1ee344
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-10 11:36:34 +0300

    Update ViewModelLocator class.
    Update UserViewModel property to return viewModel during design time.

--- Kulagin.MasteringWPF.Views/ViewModelLocators/ViewModelLocator.cs
+++ Kulagin.MasteringWPF.Views/ViewModelLocators/ViewModelLocator.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using Kulagin.MasteringWPF.Managers;
 using Kulagin.MasteringWPF.ViewModels.Interfaces;
+using Kulagin.MasteringWPF.ViewModels.Mocks;
 
 
 namespace Kulagin.MasteringWPF.Views.ViewModelLocators {
     class ViewModelLocator {
+        private DependencyObject dependencyObject = new DependencyObject();
+
+        public bool IsDesignTime {
+            get { return DesignerProperties.GetIsInDesignMode(dependencyObject); }
+        }
+
         public IUserViewModel UserViewModel {
-            get { return DependencyManager.Instance.Resolve<IUserViewModel>(); }
+            get {
+                return IsDesignTime ? new MockUserViewModel() : DependencyManager.Instance.Resolve<IUserViewModel>();
+            }
         }
     }
 }
