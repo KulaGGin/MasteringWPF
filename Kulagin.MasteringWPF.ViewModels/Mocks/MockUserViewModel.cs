639d35a8b17695014513b60d85f292339c67b189
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-10 11:35:44 +0300

    Add MockUserViewModel.

---
+++ Kulagin.MasteringWPF.ViewModels/Mocks/MockUserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Kulagin.MasteringWPF.DataModels;
+using Kulagin.MasteringWPF.ViewModels;
+using Kulagin.MasteringWPF.ViewModels.Commands;
+using Kulagin.MasteringWPF.ViewModels.Interfaces;
+
+
+namespace Kulagin.MasteringWPF.ViewModels.Mocks {
+    public class MockUserViewModel : BaseViewModel, IUserViewModel {
+        private User user = new User(Guid.NewGuid(), "James Smith", 25);
+
+        public User User {
+            get { return user; }
+            set { user = value; NotifyPropertyChanged(); }
+        }
+
+        public ICommand SaveCommand {
+            get { return new ActionCommand(action => Save()); }
+        }
+
+        private void Save() {
+            // Save User object
+        }
+    }
+}
