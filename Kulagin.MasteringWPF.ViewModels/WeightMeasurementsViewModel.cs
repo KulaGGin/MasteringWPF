38f79cf603d3f6bab7f839cc70ba2394407e9886
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-11 11:15:18 +0300

    Add WeightMeasurementsViewModel to the ViewModels project.

---
+++ Kulagin.MasteringWPF.ViewModels/WeightMeasurementsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.ViewModels {
+    public class WeightMeasurementsViewModel : BaseViewModel {
+        private List<int> weights = new List<int>() { 90, 89, 92, 91, 94, 95, 98, 99, 101 };
+        public List<int> Weights {
+            get { return weights; }
+            set { weights = value; NotifyPropertyChanged(); }
+        }
+    }
+}
