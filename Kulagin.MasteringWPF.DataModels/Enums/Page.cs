1c9000ba64ba7fd5a4076414d0ba152fea1802b0
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-11 20:26:32 +0300

    Add Page enum.

---
+++ Kulagin.MasteringWPF.DataModels/Enums/Page.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.DataModels.Enums {
+    public enum Page {
+        [Description("Bit Rate")]
+        BitRate,
+
+        [Description("Weight Measurement")]
+        WeightMeasurements,
+    }
+}
