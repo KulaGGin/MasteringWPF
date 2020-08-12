c28b5e9d3484dd0d4b87e230619cc306ad58c1f7
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-12 15:44:56 +0300

    Add AllUsersViewModel to the ViewModels project.

---
+++ Kulagin.MasteringWPF.ViewModels/AllUsersViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulagin.MasteringWPF.DataModels;
+
+
+namespace Kulagin.MasteringWPF.ViewModels {
+    /// <summary>
+    /// A View Model that is paired with the AllUsersView class, to display the various User-related examples from the book.
+    /// </summary>
+    public class AllUsersViewModel : BaseViewModel {
+        private ObservableCollection<User> users = null;
+
+        /// <summary>
+        /// Initializes a new AllUsersViewModel instance.
+        /// </summary>
+        public AllUsersViewModel() {
+            PopulateUsers();
+        }
+
+        /// <summary>
+        /// Gets or sets the collection of User objects to display in the View.
+        /// </summary>
+        public ObservableCollection<User> Users {
+            get { return users; }
+            set { if(users != value) { users = value; NotifyPropertyChanged(); } }
+        }
+
+        private void PopulateUsers() {
+            Users = new ObservableCollection<User>();
+            Users.Add(new User(Guid.NewGuid(), "James Smith", 25));
+            Users.Add(new User(Guid.NewGuid(), "Robert Johnson", 53));
+            Users.Add(new User(Guid.NewGuid(), "Maria Garcia", 32));
+        }
+
+    }
+}
