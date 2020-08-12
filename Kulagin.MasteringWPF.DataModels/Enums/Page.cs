adab8cacb780faae406b449f6c77d2b051b6d4d7
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-12 15:43:53 +0300

    Add comments for Page enum in the DataModels project.

--- Kulagin.MasteringWPF.DataModels/Enums/Page.cs
+++ Kulagin.MasteringWPF.DataModels/Enums/Page.cs
@@ -6,10 +6,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Kulagin.MasteringWPF.DataModels.Enums {
+    /// <summary>
+    /// Represents a page or View in the related Mastering Windows Presentaion Foundation demonstration application.
+    /// </summary>
     public enum Page {
+
+        /// <summary>
+        /// Represents the Bit Rate page in the related Mastering Windows Presentaion Foundation demonstration application.
+        /// </summary>
         [Description("Bit Rate")]
         BitRate,
 
+        /// <summary>
+        /// Represents the Weight Measurement page in the related Mastering Windows Presentaion Foundation demonstration application.
+        /// </summary>
         [Description("Weight Measurement")]
         WeightMeasurements,
     }
