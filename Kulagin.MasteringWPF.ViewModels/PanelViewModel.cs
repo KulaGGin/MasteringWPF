cd3cc5d46d43cf62e52705dabc50e7e35c119893
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-14 20:15:38 +0300

    Add PanelViewModel to the ViewModels project.

---
+++ Kulagin.MasteringWPF.ViewModels/PanelViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.ViewModels {
+    /// <summary>
+    /// A View Model that is paired with the PanelView class, which provides the data to recreate the various panel-related examples from the book.
+    /// </summary>
+    public class PanelViewModel : BitRateViewModel {
+    }
+}
