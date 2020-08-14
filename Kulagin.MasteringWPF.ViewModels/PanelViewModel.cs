0036d1c2630a9d616317e73c6a5e6b63eb867b4a
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-14 22:47:24 +0300

    Update PanelViewModel.
    Add field and property for days.

--- Kulagin.MasteringWPF.ViewModels/PanelViewModel.cs
+++ Kulagin.MasteringWPF.ViewModels/PanelViewModel.cs
@@ -9,5 +9,14 @@ namespace Kulagin.MasteringWPF.ViewModels {
     /// A View Model that is paired with the PanelView class, which provides the data to recreate the various panel-related examples from the book.
     /// </summary>
     public class PanelViewModel : BitRateViewModel {
+        private List<int> days = Enumerable.Range(1, 31).ToList();
+
+        /// <summary>
+        /// Gets or sets the collection of day numerals, to be displayed in the Uniform Grid example in the View.
+        /// </summary>
+        public List<int> Days {
+            get { return days; }
+            set { days = value; NotifyPropertyChanged(); }
+        }
     }
 }
