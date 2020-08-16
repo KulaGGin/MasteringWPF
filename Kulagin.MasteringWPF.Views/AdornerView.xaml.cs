f52586f2a95305646591089d9f7c8f85b7b83ded
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-16 17:05:48 +0300

    Add AdornerView to the Views project.

---
+++ Kulagin.MasteringWPF.Views/AdornerView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Kulagin.MasteringWPF.Views.Adorners;
+
+
+namespace Kulagin.MasteringWPF.Views {
+    /// <summary>
+    /// Interaction logic for AdornerView.xaml
+    /// </summary>
+    public partial class AdornerView : UserControl {
+        public AdornerView() {
+            InitializeComponent();
+            Loaded += View_Loaded;
+        }
+
+        private void View_Loaded(object sender, RoutedEventArgs e) {
+            AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(Canvas);
+            foreach(UIElement uiElement in Canvas.Children) {
+                adornerLayer.Add(new ResizeAdorner(uiElement));
+            }
+        }
+    }
+}
