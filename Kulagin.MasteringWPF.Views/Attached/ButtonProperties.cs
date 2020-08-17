9f379c818e5e197ac650f078c754178c00742f62
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-17 22:43:19 +0300

    Add ButtonProperties class to the Views project.

---
+++ Kulagin.MasteringWPF.Views/Attached/ButtonProperties.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Kulagin.MasteringWPF.Views.Attached {
+    public class ButtonProperties : DependencyObject {
+
+        public static readonly DependencyProperty StrokeThicknessProperty = DependencyProperty.RegisterAttached("StrokeThickness",
+                                                                                                                typeof(double),
+                                                                                                                typeof(ButtonProperties),
+                                                                                                                new FrameworkPropertyMetadata((double)2.0M));
+
+        public static double GetStrokeThickness(DependencyObject dependencyObject) {
+            return (double)dependencyObject.GetValue(StrokeThicknessProperty);
+        }
+
+        public static void SetStrokeThickness(DependencyObject dependencyObject, double value) {
+            dependencyObject.SetValue(StrokeThicknessProperty, value);
+        }
+
+    }
+}
