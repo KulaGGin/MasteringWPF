6fbb9e8252a0cd86dd88f95fb7ec04008c1c3f2e
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-25 23:29:07 +0300

    Add DataGridBoundTemplateColumn to the views project.

---
+++ Kulagin.MasteringWPF.Views/Controls/DataGridBoundTemplateColumn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+
+namespace Kulagin.MasteringWPF.Views.Controls {
+    public class DataGridBoundTemplateColumn : DataGridTemplateColumn {
+        public Binding Binding { get; set; }
+        protected override FrameworkElement GenerateElement(DataGridCell cell, object dataItem) {
+            FrameworkElement element = base.GenerateElement(cell, dataItem);
+
+            if(Binding != null)
+                element.SetBinding(ContentPresenter.ContentProperty, Binding);
+
+            return element;
+        }
+    }
+}
