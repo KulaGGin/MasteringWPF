cd1c7539c32bd0d48d7813fd0f86276f552ee9a0
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-25 23:29:55 +0300

    Update Spreadsheet control. override OnItemsSourceChanged method.

--- Kulagin.MasteringWPF.Views/Controls/Spreadsheet.xaml.cs
+++ Kulagin.MasteringWPF.Views/Controls/Spreadsheet.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Kulagin.MasteringWPF.DataModels;
+
 
 namespace Kulagin.MasteringWPF.Views.Controls {
     /// <summary>
@@ -20,6 +24,34 @@ namespace Kulagin.MasteringWPF.Views.Controls {
     public partial class Spreadsheet : DataGrid {
         public Spreadsheet() {
             InitializeComponent();
+        }
+
+        protected override void OnItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue) {
+            if(!(newValue is DataRowCollection rows) || rows.Count == 0)
+                return;
+
+            Cell[] cells = rows[0].ItemArray.Cast<Cell>().ToArray();
+            Columns.Clear();
+
+            DataTemplate cellTemplate = (DataTemplate)FindResource("CellTemplate");
+            for(int i = 0; i < cells.Length; i++) {
+                DataGridBoundTemplateColumn column = new DataGridBoundTemplateColumn {
+                                                             Header = GetColumnName(i + 1),
+                                                             CellTemplate = cellTemplate,
+                                                             Binding = new Binding($"[{i}]"),
+                                                             Width = cells[i].Width
+                                                         };
+                Columns.Add(column);
+            }
+        }
+        private string GetColumnName(int index) {
+            if(index <= 26)
+                return ((char)(index + 64)).ToString();
+
+            if(index % 26 == 0)
+                return string.Concat(GetColumnName(index / 26 - 1), "Z");
+
+            return string.Concat(GetColumnName(index / 26), GetColumnName(index % 26));
         }
     }
 }
