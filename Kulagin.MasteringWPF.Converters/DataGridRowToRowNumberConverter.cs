b5965f13a216d892509f2e1564c0a52761cbffde
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-25 23:32:57 +0300

    Add DataGridRowToRowNumberConverter.

---
+++ Kulagin.MasteringWPF.Converters/DataGridRowToRowNumberConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+
+namespace Kulagin.MasteringWPF.Converters {
+    [ValueConversion(typeof(DataGridRow), typeof(int))]
+    public class DataGridRowToRowNumberConverter : IValueConverter {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value is DataGridRow dataGridRow)
+                return dataGridRow.GetIndex() + 1;
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
