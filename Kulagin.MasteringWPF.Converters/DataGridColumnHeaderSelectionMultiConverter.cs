99f899965caf2ca779875c3e56dcb87c64f41c25
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-26 14:02:51 +0300

    Add DataGridColumnHeaderSelectionMultiConverter.

---
+++ Kulagin.MasteringWPF.Converters/DataGridColumnHeaderSelectionMultiConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+
+namespace Kulagin.MasteringWPF.Converters {
+    public class DataGridColumnHeaderSelectionMultiConverter : IMultiValueConverter {
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+            if (values == null || values.Count() != 2 || values.Any(v => v == null || v == DependencyProperty.UnsetValue))
+                return false;
+
+            string selectedColumnHeader = values[0].ToString();
+            string columnHeaderToCompare = values[1].ToString();
+
+            return selectedColumnHeader.Equals(columnHeaderToCompare);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
