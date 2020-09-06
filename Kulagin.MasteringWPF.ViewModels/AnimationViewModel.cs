8a4b5d3e9aa85511bd5fb4f3b97556a936ff8a14
KulaGGin <sergeant.coolagin@gmail.com>
2020-09-06 14:43:51 +0300

    Update AnimationView and AnimationViewModel. Add Validation example.

--- Kulagin.MasteringWPF.ViewModels/AnimationViewModel.cs
+++ Kulagin.MasteringWPF.ViewModels/AnimationViewModel.cs
@@ -6,5 +6,29 @@ using System.Threading.Tasks;
 
 namespace Kulagin.MasteringWPF.ViewModels {
     public class AnimationViewModel : BaseViewModel {
+        private string name = string.Empty;
+        private bool isValid = false;
+
+        /// <summary>
+        /// Gets or sets the text to be used in the Validation example in the View.
+        /// </summary>
+        public string Name {
+            get { return name; }
+            set {
+                if(name != value) {
+                    name = value;
+                    NotifyPropertyChanged();
+                    IsValid = name.Length > 2;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the Boolean flag to be used in the Validation example in the View.
+        /// </summary>
+        public bool IsValid {
+            get { return isValid; }
+            set { if(isValid != value) { isValid = value; NotifyPropertyChanged(); } }
+        }
     }
 }
