5cf87031dc646442df4ceca3ded7077af8d4e1a0
KulaGGin <sergeant.coolagin@gmail.com>
2020-09-11 19:51:57 +0300

    Update AnimatedStackPanel.
    Add animation capabilities of removing items.

--- Kulagin.MasteringWPF.Views/Panels/AnimatedStackPanel.cs
+++ Kulagin.MasteringWPF.Views/Panels/AnimatedStackPanel.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Collections.Generic;
+using Kulagin.MasteringWPF.DataModels.Enums;
+using Animatable = Kulagin.MasteringWPF.DataModels.Animatable;
+using IAnimatable = Kulagin.MasteringWPF.DataModels.Interfaces.IAnimatable;
 
 
 namespace Kulagin.MasteringWPF.Views.Panels {
     public class AnimatedStackPanel : Panel {
+
+        private List<UIElement> elementsToBeRemoved = new List<UIElement>();
 
         public static DependencyProperty OrientationProperty = DependencyProperty.Register(
                                                                                       nameof(Orientation),
@@ -51,10 +58,92 @@ namespace Kulagin.MasteringWPF.Views.Panels {
                     endPosition.Y += child.DesiredSize.Height;
                 }
 
-                AnimatePosition(child, endPosition, TimeSpan.FromMilliseconds(300));
+                BeginAnimations(child, finalSize, endPosition);
             }
 
             return finalSize;
+        }
+
+        private void BeginAnimations(UIElement child, Size finalSize, Point endPosition) {
+            FrameworkElement frameworkChild = (FrameworkElement)child;
+
+            if (frameworkChild.DataContext is IAnimatable) {
+                Animatable animatable = ((IAnimatable)frameworkChild.DataContext).Animatable;
+
+                animatable.OnRemovalStatusChanged -= Item_OnRemovalStatusChanged;
+                animatable.OnRemovalStatusChanged += Item_OnRemovalStatusChanged;
+
+                if (animatable.AdditionStatus == AdditionStatus.DoNotAnimate) {
+                    child.Arrange(new Rect(endPosition.X, endPosition.Y, frameworkChild.ActualWidth, frameworkChild.ActualHeight));
+                }
+                else if (animatable.AdditionStatus == AdditionStatus.ReadyToAnimate) {
+                    AnimateEntry(child, endPosition);
+                    animatable.AdditionStatus = AdditionStatus.Added;
+                    animatable.TransitionStatus = TransitionStatus.ReadyToAnimate;
+                }
+                else if (animatable.RemovalStatus == RemovalStatus.ReadyToAnimate)
+                    AnimateExit(child, endPosition, finalSize);
+                else if (animatable.TransitionStatus == TransitionStatus.ReadyToAnimate)
+                    AnimateTransition(child, endPosition);
+            }
+        }
+
+        private void Item_OnRemovalStatusChanged(object sender, EventArgs e) {
+            if (((Animatable)sender).RemovalStatus == RemovalStatus.ReadyToAnimate)
+                InvalidateArrange();
+        }
+
+        private void AnimateEntry(UIElement child, Point endPosition) {
+            AnimatePosition(child, endPosition, TimeSpan.FromMilliseconds(300));
+        }
+
+        private void AnimateTransition(UIElement child, Point endPosition) {
+            AnimatePosition(child, endPosition, TimeSpan.FromMilliseconds(300));
+        }
+
+        private void AnimateExit(UIElement child, Point startPosition, Size finalSize) {
+            SetZIndex(child, 100);
+            Point endPosition = new Point(startPosition.X + finalSize.Width, startPosition.Y);
+
+            AnimatePosition(child, startPosition, endPosition, TimeSpan.FromMilliseconds(300), RemovalAnimation_Completed);
+
+            elementsToBeRemoved.Add(child);
+        }
+
+        private void AnimatePosition(UIElement child, Point startPosition, Point endPosition, TimeSpan animationDuration, EventHandler animationCompletedHandler) {
+            if (startPosition.X != endPosition.X) {
+                DoubleAnimation xAnimation = new DoubleAnimation(startPosition.X, endPosition.X, animationDuration);
+
+                xAnimation.AccelerationRatio = 1.0;
+
+                if (animationCompletedHandler != null)
+                    xAnimation.Completed += animationCompletedHandler;
+
+                GetTranslateTransform(child).BeginAnimation(TranslateTransform.XProperty, xAnimation);
+            }
+
+            if (startPosition.Y != endPosition.Y) {
+                DoubleAnimation yAnimation = new DoubleAnimation(startPosition.Y, endPosition.Y, animationDuration);
+
+                yAnimation.AccelerationRatio = 1.0;
+
+                if (startPosition.X == endPosition.X && animationCompletedHandler != null)
+                    yAnimation.Completed += animationCompletedHandler;
+
+                GetTranslateTransform(child).BeginAnimation(TranslateTransform.YProperty, yAnimation);
+            }
+        }
+
+        private void RemovalAnimation_Completed(object sender, EventArgs e) {
+            for (int index = elementsToBeRemoved.Count - 1; index >= 0; index--) {
+                FrameworkElement frameworkElement = elementsToBeRemoved[index] as FrameworkElement;
+
+                if (frameworkElement.DataContext is IAnimatable) {
+                    ((IAnimatable)frameworkElement.DataContext).Animatable.RemovalStatus = RemovalStatus.ReadyToRemove;
+
+                    elementsToBeRemoved.Remove(frameworkElement);
+                }
+            }
         }
 
         private void AnimatePosition(UIElement child, Point endPosition, TimeSpan animationDuration) {
