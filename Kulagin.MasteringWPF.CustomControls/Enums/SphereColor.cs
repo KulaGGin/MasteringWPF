0c535854d63b99d699ac54973338e7ecbcd7ead1
KulaGGin <sergeant.coolagin@gmail.com>
2020-09-13 21:57:41 +0300

    Add SphereColor Enum to the CustomControls project.

---
+++ Kulagin.MasteringWPF.CustomControls/Enums/SphereColor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.CustomControls.Enums {
+    /// <summary>
+    /// Represents the available colors in the CompanyName.ApplicationName.CustomControls.GlowButton class.
+    /// </summary>
+    public enum SphereColor {
+        /// <summary>
+        /// Represents a green glow button.
+        /// </summary>
+        Green,
+        /// <summary>
+        /// Represents a red glow button.
+        /// </summary>
+        Red
+    }
+}
