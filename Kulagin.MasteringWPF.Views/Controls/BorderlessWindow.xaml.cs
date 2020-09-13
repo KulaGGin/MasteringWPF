90e4d58b986f009c3e243a9ad0c6624bdc02af31
KulaGGin <sergeant.coolagin@gmail.com>
2020-09-13 20:24:28 +0300

    Add BorderlessWindow to the Views project.

---
+++ Kulagin.MasteringWPF.Views/Controls/BorderlessWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kulagin.MasteringWPF.Views.Controls {
+    /// <summary>
+    /// Interaction logic for BorderlessWindow.xaml
+    /// </summary>
+    public partial class BorderlessWindow : Window {
+        public BorderlessWindow() {
+            InitializeComponent();
+            MouseLeftButtonDown += (o, e) => DragMove();
+        }
+    }
+}
