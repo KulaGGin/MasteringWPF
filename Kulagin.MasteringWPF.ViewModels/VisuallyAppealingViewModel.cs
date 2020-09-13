50176a0eecf9c3e77d3cbb32fb0b6032f0ce2575
KulaGGin <sergeant.coolagin@gmail.com>
2020-09-13 22:08:35 +0300

    Update VisuallyAppealingViewModel. Add InCount and OutCount properties.

--- Kulagin.MasteringWPF.ViewModels/VisuallyAppealingViewModel.cs
+++ Kulagin.MasteringWPF.ViewModels/VisuallyAppealingViewModel.cs
@@ -6,5 +6,14 @@ using System.Threading.Tasks;
 
 namespace Kulagin.MasteringWPF.ViewModels {
     public class VisuallyAppealingViewModel : BaseViewModel {
+        /// <summary>
+        /// Gets or sets the value of the fictional number of support tickets that have come in.
+        /// </summary>
+        public int InCount { get; set; } = 148;
+
+        /// <summary>
+        /// Gets or sets the value of the fictional number of support tickets that have been dealt with, or gone out.
+        /// </summary>
+        public int OutCount { get; set; } = 112;
     }
 }
