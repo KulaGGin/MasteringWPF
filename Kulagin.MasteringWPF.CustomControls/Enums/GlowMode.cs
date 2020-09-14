aabf7b2fe058ed3e67fd68585438c3afc35fb79b
KulaGGin <sergeant.coolagin@gmail.com>
2020-09-14 14:22:37 +0300

    Add GlowMode Enum to CustomControls project.

---
+++ Kulagin.MasteringWPF.CustomControls/Enums/GlowMode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.CustomControls.Enums {
+    /// <summary>
+    /// Represents the types of movement effects available in the CompanyName.ApplicationName.CustomControls.GlowButton class.
+    /// </summary>
+    public enum GlowMode {
+        /// <summary>
+        /// Represents a movement effect where the center of the gradient will not move with the mouse when it is over the control.
+        /// </summary>
+        NoCenterMovement,
+        /// <summary>
+        /// Represents a movement effect where the center of the gradient will move horiontally with the mouse when it is over the control.
+        /// </summary>
+        HorizontalCenterMovement,
+        /// <summary>
+        /// Represents a movement effect where the center of the gradient will move both horiontally and vertically with the mouse when it is over the control.
+        /// </summary>
+        FullCenterMovement
+    }
+}
