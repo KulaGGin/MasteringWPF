f20f682fb2e7178a6c6927e35c03d6db36dd2439
KulaGGin <sergeant.coolagin@gmail.com>
2020-09-30 07:49:23 +0300

    Add ProductViewModel to the ViewModels project.

---
+++ Kulagin.MasteringWPF.ViewModels/ProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulagin.MasteringWPF.DataModels;
+
+
+namespace Kulagin.MasteringWPF.ViewModels {
+    public class ProductViewModel : BaseViewModel {
+        private Product product = new Product();
+        public Product Product {
+            get { return product; }
+            set {
+                if(product != value) {
+                    product = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}
