e6d6c8c9c454ed939dac8219a45cc82189410aba
KulaGGin <sergeant.coolagin@gmail.com>
2020-09-30 18:37:56 +0300

    Add ProductsExtended collection to the DataModels project.

---
+++ Kulagin.MasteringWPF.DataModels/Collections/ProductsExtended.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.DataModels.Collections {
+    /// <summary>
+    /// Represents a collection of ProductExtended objects in the application.
+    /// </summary>
+    public class ProductsExtended : BaseCollection<ProductExtended> { }
+}
