e13c1de23fa6eee3953a7f1897cce37cd0f5b9f7
KulaGGin <sergeant.coolagin@gmail.com>
2020-10-10 18:30:35 +0300

    Add MinimumAttribute class to DataModels project.

---
+++ Kulagin.MasteringWPF.DataModels/Attributes/MinimumAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Kulagin.MasteringWPF.DataModels.Attributes {
+    public class MinimumAttribute : ValidationAttribute {
+        private double minimumValue = 0.0;
+
+        public MinimumAttribute(double minimumValue) {
+            this.minimumValue = minimumValue;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext) {
+            if ((value.GetType() != typeof(decimal) || (decimal)value < (decimal)minimumValue)) {
+                string[] memberNames = new string[] {validationContext.MemberName};
+
+                return new ValidationResult(ErrorMessage, memberNames);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
