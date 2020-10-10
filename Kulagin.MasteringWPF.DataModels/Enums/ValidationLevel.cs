ed3ba51f8fad3c186d08dcf1a8f0fea09dbe7d06
KulaGGin <sergeant.coolagin@gmail.com>
2020-10-10 19:02:16 +0300

    Add ValidationLevel enum to DataModels project.

---
+++ Kulagin.MasteringWPF.DataModels/Enums/ValidationLevel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.DataModels.Enums {
+    public enum ValidationLevel {
+        None, Partial, Full
+    }
+}
