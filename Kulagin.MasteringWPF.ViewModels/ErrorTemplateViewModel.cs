ba507d5f68a14db0a088005e5d3be56ecee573ea
KulaGGin <sergeant.coolagin@gmail.com>
2020-10-13 12:47:11 +0300

    Add ErrorTemplateViewModel to the ViewModels project.

---
+++ Kulagin.MasteringWPF.ViewModels/ErrorTemplateViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulagin.MasteringWPF.DataModels;
+
+
+namespace Kulagin.MasteringWPF.ViewModels {
+    /// <summary>
+    /// Provides a validatable ProductNotifyExtended object to highlight the validation error template.
+    /// </summary>
+    public class ErrorTemplateViewModel : BaseViewModel {
+        private ProductNotifyExtended product = new ProductNotifyExtended();
+
+        /// <summary>
+        /// Initializes a new ErrorTemplateViewModel object.
+        /// </summary>
+        public ErrorTemplateViewModel() {
+            Product.ValidateAllProperties();
+        }
+
+        /// <summary>
+        /// Gets or sets the validatable ProductNotifyExtended object to be edited in the View.
+        /// </summary>
+        public ProductNotifyExtended Product {
+            get { return product; }
+            set { if(product != value) { product = value; NotifyPropertyChanged(); } }
+        }
+    }
+}
