06bb1a29477030eb9db7a12a6264a2e7336f7215
KulaGGin <sergeant.coolagin@gmail.com>
2020-10-15 15:38:18 +0300

    Add FeedbackCollection to DataModels project.

---
+++ Kulagin.MasteringWPF.DataModels/Collections/FeedbackCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Kulagin.MasteringWPF.DataModels.Collections {
+    public class FeedbackCollection : BaseAnimatableCollection<Feedback> {
+        public FeedbackCollection(IEnumerable<Feedback> feedbackCollection) :
+            base(feedbackCollection) {
+        }
+
+        public FeedbackCollection() : base() {
+        }
+
+        public new void Add(Feedback feedback) {
+            if (!string.IsNullOrEmpty(feedback.Message)
+                && (Count == 0 || this.All(f => f.Message != feedback.Message)))
+                base.Add(feedback);
+        }
+
+        public void Add(string message, bool isSuccess) {
+            Add(new Feedback(message, isSuccess));
+        }
+    }
+}
