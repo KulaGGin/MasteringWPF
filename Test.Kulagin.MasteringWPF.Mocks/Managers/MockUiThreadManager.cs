694269fdb58da78bcb6cb94c72a6b8719c640832
KulaGGin <sergeant.coolagin@gmail.com>
2020-10-16 01:47:08 +0300

    Add MockUiThreadManager to the Mocks project.

---
+++ Test.Kulagin.MasteringWPF.Mocks/Managers/MockUiThreadManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulagin.MasteringWPF.Managers.Interfaces;
+
+
+namespace Test.Kulagin.MasteringWPF.Mocks.Managers {
+    public class MockUiThreadManager : IUiThreadManager {
+        public object RunOnUiThread(Delegate method) {
+            return method.DynamicInvoke();
+        }
+
+        public Task RunAsynchronously(Action method) {
+            Task task = new Task(method);
+            task.RunSynchronously();
+            return task;
+        }
+
+        public Task<TResult> RunAsynchronously<TResult>(Func<TResult> method) {
+            Task<TResult> task = new Task<TResult>(method);
+            task.RunSynchronously();
+            return task;
+        }
+    }
+}
