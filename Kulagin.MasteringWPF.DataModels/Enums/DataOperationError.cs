d7aab184ab9c42efabf626070dea3b72375d4384
KulaGGin <sergeant.coolagin@gmail.com>
2020-10-16 00:54:15 +0300

    Add DataOperationError enum to DataModels project.

---
+++ Kulagin.MasteringWPF.DataModels/Enums/DataOperationError.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.DataModels.Enums {
+    public enum DataOperationError {
+        [Description("")]
+        None = 0,
+        [Description("A database constraint has not been adhered to, so this operation cannot be completed")]
+        DatabaseConstraintError = 9995,
+        [Description("There was an undetermined data operation error")]
+        UndeterminedDataOperationError = 9997,
+        [Description("There was a problem connecting to the database")]
+        DatabaseConnectionError = 9998,
+    }
+}
