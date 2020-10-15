aaedcd85822bbb73862f85c901780f54961ba6a9
KulaGGin <sergeant.coolagin@gmail.com>
2020-10-16 01:25:15 +0300

    Add SetDataOperationResult to the DataModels project.

---
+++ Kulagin.MasteringWPF.DataModels/SetDataOperationResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.DataModels {
+    public class SetDataOperationResult : DataOperationResult<bool> {
+
+        public SetDataOperationResult(Exception exception, string errorText) :
+            base(exception, errorText) {
+
+        }
+
+        public SetDataOperationResult(string successText) :
+            base(successText) {
+
+        }
+    }
+}
