dc1d6c484b1cf5aefc58adc1ed8609461f75f1ba
KulaGGin <sergeant.coolagin@gmail.com>
2020-10-16 01:45:08 +0300

    Add UiThreadManager to Managers project.

---
+++ Kulagin.MasteringWPF.Managers/UiThreadManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using Kulagin.MasteringWPF.Managers.Interfaces;
+
+namespace Kulagin.MasteringWPF.Managers {
+    public class UiThreadManager : IUiThreadManager {
+        public object RunOnUiThread(Delegate method) {
+            return Application.Current.Dispatcher.Invoke(DispatcherPriority.Normal, method);
+        }
+        public Task RunAsynchronously(Action method) {
+            return Task.Run(method);
+        }
+        public Task<TResult> RunAsynchronously<TResult>(Func<TResult> method) {
+            return Task.Run(method);
+        }
+    }
+}
