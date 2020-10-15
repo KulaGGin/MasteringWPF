38cce66b259fc3f88d07861843be3f83060c593a
KulaGGin <sergeant.coolagin@gmail.com>
2020-10-16 01:19:33 +0300

    Add GetDataOperationResult to the DataModels project.

---
+++ Kulagin.MasteringWPF.DataModels/GetDataOperationResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.DataModels {
+    public class GetDataOperationResult<T> : DataOperationResult<T> {
+        public GetDataOperationResult(Exception exception, string errorText) :
+            base(exception, errorText) {
+            ReturnValue = default(T);
+        }
+
+        public GetDataOperationResult(Exception exception) :
+            this(exception, string.Empty) {
+
+        }
+        public GetDataOperationResult(T returnValue, string successText) :
+            base(successText) {
+            ReturnValue = returnValue;
+        }
+
+        public GetDataOperationResult(T returnValue) :
+            this(returnValue, string.Empty) {
+
+        }
+
+        public T ReturnValue { get; private set; }
+    }
+}
