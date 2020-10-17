d8abf55054d9c67f5cd1c5a7c9f7067f06f68b35
KulaGGin <sergeant.coolagin@gmail.com>
2020-10-17 19:07:19 +0300

    Add IntegerExtensions class to the Extensions project.

---
+++ Kulagin.MasteringWPF.Extensions/IntegerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kulagin.MasteringWPF.Extensions {
+    public static class IntegerExtensions {
+        public static string Pluralize(this int input, string wordToAdjust) {
+            return $"{wordToAdjust}{(input == 1 ? string.Empty : "s")}";
+        }
+
+        public static string Combine(this int input, string wordToAdjust) {
+            return $"{input} {wordToAdjust}{(input == 1 ? string.Empty : "s")}";
+        }
+    }
+}
